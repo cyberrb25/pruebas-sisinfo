a1c77b9979cff56fc937290a87affa73a8d6d40d
uo291750 <uo291750@uniovi.es>
2024-02-11 22:13:55 +0100

    Edición en la rama de master, habiendo creado una rama local

--- demo1/src/demo/Demo.java
+++ demo1/src/demo/Demo.java
@@ -1,7 +1,9 @@
 package demo;
 
 public class Demo {
+	/** Esto en realidad es un comentario realizado en la rama de master */
 	private int variable1;
+	
 	
 	public Demo(int v1){
 		variable1 = v1;
